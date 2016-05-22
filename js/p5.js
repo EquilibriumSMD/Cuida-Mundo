f05622b2f34da56fa9aecbb19ad1143942956531
A11v1r15 <a11v1r15@Rocketmail.com>
2016-05-22 17:57:51 -0300

    Bonequinho

--- js/p5.js
+++ js/p5.js
@@ -2,6 +2,8 @@ var canvas;
 var tiles = [];
 var img = [];
 var lixo = [];
+var bonequinho;
+
 
 function setup() {
   canvas = createCanvas(1680, 925);
@@ -10,15 +12,16 @@ function setup() {
     img[x] = [];
     for (y = 0; y < 24; y++) {
       img[x][y] = createImg("../refactored-spork/img/test" + floor((noise(x, y) * 3) + 1) + ".png");
-      img[x][y].position(windowWidth / 2 - 32 * x + 32 * y, 64 + 16 * x + 16 * y);
+      img[x][y].position(treesholdX(x, y), treesholdY(x, y));
     }
   }
   for (x = 0; x < 24; x++) {
     lixo[x] = [];
     for (y = 0; y < 24; y++) {
-      lixo[x][y] = floor((random(10)));
+      lixo[x][y] = floor(random(10));
     }
   }
+  bonequinho = new p5.Vector(12, 12);
   blendMode(REPLACE);
   canvas.remove();
   canvas.parent(document.body);
@@ -26,8 +29,19 @@ function setup() {
 }
 
 function draw() {
+  clear();
   background(0, 0);
   drawLixo();
+  drawbonequinho();
+}
+
+function drawbonequinho() {
+  fill(0, 127, 255);
+  triangle(treesholdX(bonequinho.x, bonequinho.y) + 17, treesholdY(bonequinho.x, bonequinho.y) - 17,
+    treesholdX(bonequinho.x, bonequinho.y) + 47, treesholdY(bonequinho.x, bonequinho.y) - 17,
+    treesholdX(bonequinho.x, bonequinho.y) + 32, treesholdY(bonequinho.x, bonequinho.y) + 8);
+  fill(255, 127, 0);
+  ellipse(treesholdX(bonequinho.x, bonequinho.y) + 32, treesholdY(bonequinho.x, bonequinho.y) - 30, 20, 20);
 }
 
 function drawLixo() {
@@ -36,23 +50,23 @@ function drawLixo() {
       switch (lixo[x][y]) {
         case 1:
           fill(255, 25, 25);
-          ellipse(windowWidth / 2 - 32 * x + 32 * y + 32, 64 + 16 * x + 16 * y + 8, 10, 10);
+          ellipse(treesholdX(x, y) + 32, treesholdY(x, y) + 8, 10, 10);
           break;
         case 2:
           fill(25, 255, 25);
-          ellipse(windowWidth / 2 - 32 * x + 32 * y + 32, 64 + 16 * x + 16 * y + 8, 10, 10);
+          ellipse(treesholdX(x, y) + 32, treesholdY(x, y) + 8, 10, 10);
           break;
         case 3:
           fill(25, 25, 255);
-          ellipse(windowWidth / 2 - 32 * x + 32 * y + 32, 64 + 16 * x + 16 * y + 8, 10, 10);
+          ellipse(treesholdX(x, y) + 32, treesholdY(x, y) + 8, 10, 10);
           break;
         case 4:
           fill(255, 255, 25);
-          ellipse(windowWidth / 2 - 32 * x + 32 * y + 32, 64 + 16 * x + 16 * y + 8, 10, 10);
+          ellipse(treesholdX(x, y) + 32, treesholdY(x, y) + 8, 10, 10);
           break;
         case 5:
           fill(25, 25, 25);
-          ellipse(windowWidth / 2 - 32 * x + 32 * y + 32, 64 + 16 * x + 16 * y + 8, 10, 10);
+          ellipse(treesholdX(x, y) + 32, treesholdY(x, y) + 8, 10, 10);
           break;
         default:
           lixo[x][y] = 0;
@@ -60,4 +74,29 @@ function drawLixo() {
       }
     }
   }
+}
+
+function keyPressed() {
+  switch (keyCode) {
+    case UP_ARROW:
+      bonequinho.y++;
+      break;
+    case DOWN_ARROW:
+      bonequinho.y--;
+      break;
+    case LEFT_ARROW:
+      bonequinho.x--;
+      break;
+    case RIGHT_ARROW:
+      bonequinho.x++;
+      break;
+  }
+}
+
+function treesholdX(x, y) {
+  return windowWidth / 2 - 32 * x + 32 * y;
+}
+
+function treesholdY(x, y) {
+  return 64 + 16 * x + 16 * y;
 }
