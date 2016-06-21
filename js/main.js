cc9ce5db7759dc7de8e7d1802ce92c3744747cf9
A11v1r15 <a11v1r15@Rocketmail.com>
2016-06-21 00:14:08 -0300

    Tela ajustada

--- js/main.js
+++ js/main.js
@@ -15,6 +15,8 @@ var holdOnload;
 createjs.Ticker.addEventListener("tick", draw);
 
 function setup() {
+  document.getElementById("defaultCanvas0").width = screen.width;
+  document.getElementById("defaultCanvas0").height = screen.height;
   canvas =  new createjs.Stage("defaultCanvas0");
   for (x = 0; x < 24; x++) {
     tiles[x] = [];
