b431bb9a6a625a66adfa452d6ea7f631f8b43460
A11v1r15 <a11v1r15@Rocketmail.com>
2016-06-22 12:31:43 -0300

    Pausa pro almoço

--- js/class.js
+++ js/class.js
@@ -1,126 +1,140 @@
 function treesholdX(y, x) {
-  return Math.floor(window.innerWidth / 2 - tSize / 2 * x + tSize / 2 * y);
+    return Math.floor(window.innerWidth / 2 - tSize / 2 * x + tSize / 2 * y);
 }
 
 function treesholdY(y, x, z) {
-  return Math.floor(tSize + tSize / 4 * x + tSize / 4 * y - z * tSize / 2.4);
+    return Math.floor(tSize + tSize / 4 * x + tSize / 4 * y - z * tSize / 2.4);
 }
 
 function Boneco() {
-  this.x = 12;
-  this.y = 12;
-  this.z = 1;
-  this.plast = 0;
-  this.vidro = 0;
-  this.papel = 0;
-  this.metal = 0;
-  this.lixoC = 0;
-  this.total = 0;
-  this.tile = new Tile("player", 0);
-    
-//animacao    
-var spritePersonagem = new createjs.SpriteSheet({
-    images: [ "img/sprite-down.png", "img/sprite-left.png", "img/sprite-right.png", "img/sprite-up.png" ],
-    frames: { width: 59, height: 64 },
-    animations: {
-				"idleD": 1,
-				"down": [0, 2, "idleD", 3],
-				"idleL": 4,
-				"left": [3, 5, "idleL", 3],
-				"idleR": 7,
-				"right": [6, 8, "idleR", 3],
-				"idleU": 10,
-				"up": [9, 11, "idleU", 3]
-    }
-});
+    this.x = 12;
+    this.y = 12;
+    this.z = 1;
+    this.plast = 0;
+    this.vidro = 0;
+    this.papel = 0;
+    this.metal = 0;
+    this.lixoC = 0;
+    this.total = 0;
 
-  this.tile.img = new createjs.Sprite(spritePersonagem, "idleD");
-  this.tile.img.framerate = 30;
-     
+    //animacao    
+    var spritePersonagem = new createjs.SpriteSheet({
+        images: ["img/sprite-down.png", "img/sprite-left.png", "img/sprite-right.png", "img/sprite-up.png"],
+        frames: {
+            width: 59,
+            height: 64
+        },
+        animations: {
+            "idleD": 1,
+            "down": [0, 2, "idleD", 3],
+            "idleL": 4,
+            "left": [3, 5, "idleL", 3],
+            "idleR": 7,
+            "right": [6, 8, "idleR", 3],
+            "idleU": 10,
+            "up": [9, 11, "idleU", 3]
+        }
+    });
 
-  this.up = function() {
-    if (this.y > 0 && tiles[this.x][this.y-1][this.z-1].tType == "floor" && tiles[this.x][this.y-1][this.z].tType != "wall")
-      this.y--;
-      createjs.Tween.get(this.tile.img, { loop: false }).to({ x: treesholdX(this.x, this.y), y: treesholdY(this.x, this.y, this.z+0.7) }, 100, createjs.Ease.getPowInOut(4));
-  }
-  this.down = function() {
-    if (this.y < 23 && tiles[this.x][this.y+1][this.z-1].tType == "floor" && tiles[this.x][this.y+1][this.z].tType != "wall")
-      this.y++;
-      createjs.Tween.get(this.tile.img, { loop: false }).to({ x: treesholdX(this.x, this.y), y: treesholdY(this.x, this.y, this.z+0.7) }, 100, createjs.Ease.getPowInOut(4));
-  }
-  this.left = function() {
-    if (this.x > 0 && tiles[this.x-1][this.y][this.z-1].tType == "floor" && tiles[this.x-1][this.y][this.z].tType != "wall")
-      this.x--;
-      createjs.Tween.get(this.tile.img, { loop: false }).to({ x: treesholdX(this.x, this.y), y: treesholdY(this.x, this.y, this.z+0.7) }, 100, createjs.Ease.getPowInOut(4));
-  }
-  this.right = function() {
-    if (this.x < 23 && tiles[this.x+1][this.y][this.z-1].tType == "floor" && tiles[this.x+1][this.y][this.z].tType != "wall")
-      this.x++;
-      createjs.Tween.get(this.tile.img, { loop: false }).to({ x: treesholdX(this.x, this.y), y: treesholdY(this.x, this.y, this.z+0.7) }, 100, createjs.Ease.getPowInOut(4));
-  }
-  this.action = function() {
-    if (tiles[this.x][this.y][this.z].tType == "lixo") {
-      switch (tiles[this.x][this.y][this.z].tId) {
-        case 1:
-          this.plast++;
-          break;
-        case 2:
-          this.vidro++;
-          break;
-        case 3:
-          this.papel++;
-          break;
-        case 4:
-          this.metal++;
-          break;
-        case 5:
-          this.lixoC++;
-          break;
-      }
-      tiles[this.x][this.y][this.z].remove();
-      this.total++;
-      this.score();
+    this.sprite = new createjs.Sprite(spritePersonagem, "idleD");
+    this.sprite.framerate = 30;
+
+
+    this.up = function() {
+        if (this.y > 0 && tiles[this.x][this.y - 1][this.z - 1].tType == "floor" && tiles[this.x][this.y - 1][this.z].tType != "wall")
+            this.y--;
+		    this.tween();
     }
-  }
-  this.score = function() {
-    lixoC.innerHTML = this.total;
-  }
+    this.down = function() {
+        if (this.y < 23 && tiles[this.x][this.y + 1][this.z - 1].tType == "floor" && tiles[this.x][this.y + 1][this.z].tType != "wall")
+            this.y++;
+		    this.tween();
+    }
+    this.left = function() {
+        if (this.x > 0 && tiles[this.x - 1][this.y][this.z - 1].tType == "floor" && tiles[this.x - 1][this.y][this.z].tType != "wall")
+            this.x--;
+		    this.tween();
+    }
+    this.right = function() {
+        if (this.x < 23 && tiles[this.x + 1][this.y][this.z - 1].tType == "floor" && tiles[this.x + 1][this.y][this.z].tType != "wall")
+            this.x++;
+		    this.tween();
+    }
+	this.tween = function() {
+        createjs.Tween.get(this.sprite, {
+            loop: false
+        }).to({
+            x: treesholdX(this.x, this.y),
+            y: treesholdY(this.x, this.y, this.z + 1.5)
+        }, 100, createjs.Ease.getPowInOut(2));
+	}
+    this.action = function() {
+        if (tiles[this.x][this.y][this.z].tType == "lixo") {
+            switch (tiles[this.x][this.y][this.z].tId) {
+                case 1:
+                    this.plast++;
+                    break;
+                case 2:
+                    this.vidro++;
+                    break;
+                case 3:
+                    this.papel++;
+                    break;
+                case 4:
+                    this.metal++;
+                    break;
+                case 5:
+                    this.lixoC++;
+                    break;
+            }
+            tiles[this.x][this.y][this.z].remove();
+            this.total++;
+            this.score();
+        }
+    }
+    this.score = function() {
+        lixoC.innerHTML = this.total;
+    }
 }
 
 function ButtonHandler() {
 
-  this.up = function() {
-    bonequinho.up();
-    bonequinho.tile.img.gotoAndPlay("up");
-  }
-  this.down = function() {
-    bonequinho.down();
-    bonequinho.tile.img.gotoAndPlay("down");
-  }
-  this.left = function() {
-    bonequinho.left();
-    bonequinho.tile.img.gotoAndPlay("left");
-  }
-  this.right = function() {
-    bonequinho.right();
-    bonequinho.tile.img.gotoAndPlay("right");
-  }
-  this.action = function() {
-    bonequinho.action();
-  }
+    this.up = function() {
+        equi.up();
+        equi.sprite.gotoAndPlay("up");
+    }
+    this.down = function() {
+        equi.down();
+        equi.sprite.gotoAndPlay("down");
+    }
+    this.left = function() {
+        equi.left();
+        equi.sprite.gotoAndPlay("left");
+    }
+    this.right = function() {
+        equi.right();
+        equi.sprite.gotoAndPlay("right");
+    }
+    this.action = function() {
+        equi.action();
+    }
 }
 
-function Tile(type, id) {
-  this.tType = type;
-  this.tId = id;
-  this.img =  new createjs.Bitmap("img/" + this.tType + this.tId + ".png");
-  if (this.tType == "void") {
-    this.tId = 0;
-  }
+function Tile(type, id, x, y, z) {
+    this.tType = type;
+    this.tId = id;
+    if (this.tType == "void") {
+        this.tId = 0;
+    } else {
+		this.img = new createjs.Bitmap("img/" + this.tType + this.tId + ".png");
+        this.img.x = treesholdX(x, y);
+        this.img.y = treesholdY(x, y, 1);
+        stage.addChild(this.img);
+	}
 
-  this.remove = function() {
-    this.tId = 0;
-	this.tType = "void";
-    stage.removeChild(this.img);
-  }
+    this.remove = function() {
+        this.tId = 0;
+        this.tType = "void";
+        stage.removeChild(this.img);
+    }
 }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var stage;
 var tiles = [];
-var bonequinho;
+var equi;
 var tSize;
 var handler;
 var buttonUp
@@ -16,94 +16,84 @@ var personagem;
 createjs.Ticker.addEventListener("tick", draw);
 
 function setup() {
-  document.getElementById("defaultCanvas0").width = window.innerWidth;
-  document.getElementById("defaultCanvas0").height = window.innerHeight;
-  stage =  new createjs.Stage("defaultCanvas0");
-  for (x = 0; x < 24; x++) {
-    tiles[x] = [];
-    for (y = 0; y < 24; y++) {
-      tiles[x][y] = [];
-      if (Math.floor(Math.random()*4) === 0) {
-        tiles[x][y][0] = new Tile("void", 0);
-      } else {
-        tiles[x][y][0] = new Tile("floor", 0);
-      }
-      stage.addChild(tiles[x][y][0].img);
-		tiles[x][y][0].img.x = treesholdX(x, y);
-		tiles[x][y][0].img.y = treesholdY(x, y, 0);
-      if (Math.floor(Math.random()*10) < 5 && tiles[x][y][0].tType != "void") {
-        tiles[x][y][1] = new Tile("lixo", Math.floor(Math.random()*5 + 1));
-		tiles[x][y][1].img.x = treesholdX(x, y);
-		tiles[x][y][1].img.y = treesholdY(x, y, 1);
-        stage.addChild(tiles[x][y][1].img);
-        total++;
-      } else {
-        tiles[x][y][1] = new Tile("void", 0);
-		tiles[x][y][1].img.x = treesholdX(x, y);
-		tiles[x][y][1].img.y = treesholdY(x, y, 1);
-        stage.addChild(tiles[x][y][1].img);
-      }
+    stage = new createjs.Stage("defaultCanvas0");
+	stage.canvas.width = window.innerWidth;
+	stage.canvas.height = window.innerHeight;
+	//Cria a Matriz de espaços vazios
+    for (x = 0; x < 24; x++) {
+        tiles[x] = [];
+        for (y = 0; y < 24; y++) {
+            tiles[x][y] = [];
+            for (z = 0; z < 24; z++) {
+                tiles[x][y][z] = new Tile("void", 0, x, y, z);
+            }
+        }
     }
-  }
-  holdOnload = true;
+    //tiles[x][y][1] = new Tile("lixo", Math.floor(Math.random() * 5 + 1), x, y, z);
+    for (x = 10; x < 20; x++) {
+        for (y = 10; y < 20; y++) {
+            tiles[x][y][0] = new Tile("floor", 0, x, y, z);
+        }
+    }
+    holdOnload = true;
 }
 
 window.onload = function() {
-  tSize = 64;
-  bonequinho = new Boneco();
-  setup();
-  if (holdOnload) {
-    buttonUp = document.getElementById("bt-Top");
-    buttonDown = document.getElementById("bt-Bottom");
-    buttonLeft = document.getElementById("bt-Left");
-    buttonRight = document.getElementById("bt-Right");
-    buttonAction = document.getElementById("bt-Coletar");
-    lixoC = document.getElementById("bt-lixo1").getElementsByTagName("strong")[0];
-    lixoT = document.getElementById("bt-lixo1").getElementsByTagName("span")[0];
-    lixoT.innerHTML = "/" + total;
-    handler = new ButtonHandler();
-    if ("true" === undefined) {
-      buttonUp.remove();
-      buttonDown.remove();
-      buttonLeft.remove();
-      buttonRight.remove();
-      buttonAction.remove();
-    } else {
-      buttonUp.onclick = handler.up;
-      buttonDown.onclick = handler.down;
-      buttonLeft.onclick = handler.left;
-      buttonRight.onclick = handler.right;
-      buttonAction.onclick = handler.action;
+    tSize = 64;
+    equi = new Boneco();
+    setup();
+    if (holdOnload) {
+        buttonUp = document.getElementById("bt-Top");
+        buttonDown = document.getElementById("bt-Bottom");
+        buttonLeft = document.getElementById("bt-Left");
+        buttonRight = document.getElementById("bt-Right");
+        buttonAction = document.getElementById("bt-Coletar");
+        lixoC = document.getElementById("bt-lixo1").getElementsByTagName("strong")[0];
+        lixoT = document.getElementById("bt-lixo1").getElementsByTagName("span")[0];
+        lixoT.innerHTML = "/" + total;
+        handler = new ButtonHandler();
+        if ("true" === undefined) {
+            buttonUp.remove();
+            buttonDown.remove();
+            buttonLeft.remove();
+            buttonRight.remove();
+            buttonAction.remove();
+        } else {
+            buttonUp.onclick = handler.up;
+            buttonDown.onclick = handler.down;
+            buttonLeft.onclick = handler.left;
+            buttonRight.onclick = handler.right;
+            buttonAction.onclick = handler.action;
+        }
+        equi.score();
+        equi.sprite.x = treesholdX(12, 12);
+        equi.sprite.y = treesholdY(12, 12, 2.5);
+        stage.addChild(equi.sprite);
     }
-    bonequinho.score();
-	bonequinho.tile.img.x = treesholdX(12, 12);
-	bonequinho.tile.img.y = treesholdY(12, 12, 1.7);
-    stage.addChild(bonequinho.tile.img);
-  }
 }
 
 function draw() {
-  stage.update();
+    stage.update();
 }
 
 document.addEventListener('keydown', keyPressed);
-	
+
 function keyPressed(event) {
-  switch (event.keyCode) {
-    case 40:
-      handler.down();
-      break;
-    case 38:
-      handler.up();
-      break;
-    case 37:
-      handler.left();
-      break;
-    case 39:
-      handler.right();
-      break;
-  }
-  if (event.key == " ") {
-    handler.action();
-  }
+    switch (event.keyCode) {
+        case 40:
+            handler.down();
+            break;
+        case 38:
+            handler.up();
+            break;
+        case 37:
+            handler.left();
+            break;
+        case 39:
+            handler.right();
+            break;
+    }
+    if (event.key == " ") {
+        handler.action();
+    }
 }
