89ee8dd7e79f53a5fc307df35bdbcc968314a2e0
isabelasilveira <isabela.wdev@gmail.com>
2016-06-25 00:41:27 -0300

    Menu funcionando

--- js/menu.js
+++ js/menu.js
@@ -6,60 +6,75 @@ var btJogar;
 var btInstrucoes;
 var btCreditos;
 var btEcopontos;
+var stage;
 
-function setup() {  
-    tela = createCanvas(1680, 925);
-    
-    home();
-}
+createjs.Ticker.addEventListener("tick", draw);
 
 function opMenu(){
     
-    document.getElementById("bt-home").onclick = home;
-    document.getElementById("bt-play").onclick = play;
-    document.getElementById("bt-instrucoes").onclick = instrucoes;
-    document.getElementById("bt-creditos").onclick = creditos;
-    document.getElementById("bt-ecopontos").onclick = ecopontos;
+//    document.getElementById("bt-home").onclick = home;
+//    document.getElementById("bt-play").onclick = play;
+//    document.getElementById("bt-instrucoes").onclick = instrucoes;
+//    document.getElementById("bt-creditos").onclick = creditos;
+//    document.getElementById("bt-ecopontos").onclick = ecopontos;
+    
+    tela = document.getElementsByTagName("body");
+    console.log(tela);
+    
+    
+    btJogar = new createjs.Bitmap("img/btJogar.png");
+    btInstrucoes = new createjs.Bitmap("img/btInstrucoes.png");
+    btCreditos = new createjs.Bitmap("img/btCreditos.png");
+    
+    //jogando as funções de cada botão
+    btJogar.addEventListener("click", play);
+    btInstrucoes.addEventListener("click", instrucoes );
+    btCreditos.addEventListener("click", creditos);
+    
+    //posicionamento dos botoes
+    btJogar.x = 300;
+    btJogar.y = 600;
+    btInstrucoes.x = 500;
+    btInstrucoes.y = 600;
+    btCreditos.x = 700;
+    btCreditos.y = 600;
+    
+    //adicionando os botões
+    stage.addChild(btJogar);
+    stage.addChild(btInstrucoes);
+    stage.addChild(btCreditos);
+    
 }
 
-function home(){
-    tela.class('home');
-    
-    //voltar ao início
-    btHome = createButton("Início").class("bt-menu").id("bt-home");
-    btHome.position(20, 19);
-    
-    //botão play
-    btJogar = createButton("Play").class("bt-menu").id("bt-play");
-    btJogar.position(100, 19);
-    
-    //botão Instruções
-    btinstrucoes = createButton("Instruções").class("bt-menu").id("bt-instrucoes");
-    btinstrucoes.position(180, 19);
-    
-    //botão Créditos
-    btCreditos = createButton("Créditos").class("bt-menu").id("bt-creditos");
-    btCreditos.position(280, 19);
-    
-    //botão Ecopontos
-    btEcopontos = createButton("Ecopontos").class("bt-menu").id("bt-ecopontos");
-    btEcopontos.position(380, 19);
+function setupMenu() {  
+    stage = new createjs.Stage("defaultCanvas0");
+	stage.canvas.width = window.innerWidth;
+	stage.canvas.height = window.innerHeight;
     
     opMenu();
 }
 
+window.onload = function() {
+    setupMenu();
+}
+
+
+
+function draw() {
+    stage.update();
+}
+
 function play(){
-    tela.class('cenario-play');
+    bgPlay = new createjs.Bitmap("img/bgPlay.png");
+    stage.addChild(bgPlay);
 }
 
 function instrucoes(){
-    tela.class('cenario-instrucoes');
+    bgInstrucoes = new createjs.Bitmap("img/bgInstrucoes.png");
+    stage.addChild(bgInstrucoes);
 }
 
 function creditos(){
-    tela.class('cenario-creditos');
-}
-
-function ecopontos(){
-    tela.class('cenario-ecopontos');
+    bgCreditos = new createjs.Bitmap("img/bgCreditos.png");
+    stage.addChild(bgCreditos);
 }
