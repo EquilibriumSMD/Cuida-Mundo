618830c190e27c0a977415ad4631d048324a66db
isabelasilveira <isabela.wdev@gmail.com>
2016-06-29 15:04:42 -0300

    Fade In

--- js/menu.js
+++ js/menu.js
@@ -24,10 +24,19 @@ function opMenu() {
     btnMais.x = 980;
     btnMais.y = 370;
 
+    //fadeIn
+    btnEco.alpha = 0;
+    btnPlay.alpha = 0;
+    btnMais.alpha = 0;
+    
     //adicionando os botões
     stage.addChild(btnEco);
     stage.addChild(btnPlay);
     stage.addChild(btnMais);
+    
+    createjs.Tween.get(btnEco).to({alpha: 1},600);
+    createjs.Tween.get(btnPlay).to({alpha: 1},600);
+    createjs.Tween.get(btnMais).to({alpha: 1},600);
 
 }
 
@@ -37,7 +46,9 @@ function setupMenu() {
     menuBG = new createjs.Bitmap(sonGoqueue.getResult("menuBG"));
     menuBG.x = 0;
     menuBG.y = 0;
+    menuBG.alpha = 0;
     stage.addChild(menuBG);
+    createjs.Tween.get(menuBG).to({alpha: 1},600);
     inGame = false;
     inMenu = true;
     opMenu();
@@ -55,6 +66,7 @@ function play() {
     bgPlay = new createjs.Bitmap(sonGoqueue.getResult("bgPlay"));
     bgPlay.x = 0;
     bgPlay.y = 0;
+    bgPlay.alpha = 0;
     if (!archivement) {
         var n = new Notification("Archievement Get!", {
             body: "First Step +50G\nSeu espírito de protetor ambiental é forte, mas o jogo ainda não está pronto ☹",
@@ -65,6 +77,7 @@ function play() {
     inGame = true;
     inMenu = false;
     stage.addChild(bgPlay);
+    createjs.Tween.get(bgPlay).to({alpha: 1},600);
     mainGame();
     btVoltarAdd();
 }
@@ -73,9 +86,11 @@ function mais() {
     bgMais = new createjs.Bitmap(sonGoqueue.getResult("bgMais"));
     bgMais.x = 0;
     bgMais.y = 0;
+    bgMais.alpha = 0;
     inMenu = false;
 
     stage.addChild(bgMais);
+    createjs.Tween.get(bgMais).to({alpha: 1},600);
     btVoltarAdd();
 }
 
@@ -83,8 +98,10 @@ function eco() {
     bgEco = new createjs.Bitmap(sonGoqueue.getResult("bgEco"));
     bgEco.x = 0;
     bgEco.y = 0;
+    bgEco.alpha = 0;
     inMenu = false;
 
     stage.addChild(bgEco);
+    createjs.Tween.get(bgEco).to({alpha: 1},600);
     btVoltarAdd();
 }
